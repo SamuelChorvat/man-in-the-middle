cd8b87ace0893162bd3f7321622966280511eda2
Samuel Chorvat <samchorvat@gmail.com>
2020-11-13 13:14:45 +0000

    modulo arthmetic puzzle finished

---
+++ Man In The Middle/Assets/Scripts/ModuloNumber.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using DG.Tweening;
+
+public class ModuloNumber : MonoBehaviour
+{
+    public string number;
+    private bool solved = false;
+    public RevealContinueButton but;
+
+    public GameObject next = null;
+    public DOTweenAnimation nextAnim = null;
+    
+    void Update() {
+        if (this.gameObject.GetComponent<TMP_InputField>().text.Equals(number, StringComparison.OrdinalIgnoreCase) && !solved) {
+            this.gameObject.GetComponent<TMP_InputField>().interactable = false;
+            this.gameObject.GetComponent<Image>().enabled = false;
+            this.gameObject.transform.Find("Text Area").Find("Text").GetComponent<TextMeshProUGUI>().color = Color.green;
+            solved = true;
+            
+            if (next == null || nextAnim == null) {
+                but.StartReveal();
+            } else {
+                next.SetActive(true);
+                nextAnim.DORestart();
+            }
+
+        }
+    }
+}
---
+++ Man In The Middle/Assets/Scripts/ModuloPuzzleControl.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuloPuzzleControl : MonoBehaviour
+{
+    public GameObject firstNumber;
+    public DOTweenAnimation firstNumberAnim;
+
+    public void DisplayFirstNo() {
+        firstNumber.SetActive(true);
+        firstNumberAnim.DORestart();
+    }
+}
