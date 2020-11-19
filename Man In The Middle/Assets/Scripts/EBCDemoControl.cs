71ea0e95f912e58c572e03eea52175d7eeecf357
Samuel Chorvat <samchorvat@gmail.com>
2020-11-19 12:23:08 +0000

    ECB picture demo

---
+++ Man In The Middle/Assets/Scripts/EBCDemoControl.cs
@@ -0,0 +1,14 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EBCDemoQuest : MonoBehaviour
+{
+    public GameObject demoObj;
+
+    public void StartDemo() {
+        demoObj.SetActive(true);
+        this.gameObject.GetComponent<DOTweenAnimation>().DORestart();
+    }
+}
