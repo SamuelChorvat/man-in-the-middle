4e9e7908107c19d6592813f0c49cfb456f30b307
Samuel Chorvat <samchorvat@gmail.com>
2020-11-25 19:05:18 +0000

    added XOR and CTR section, ready to move on public key enc

---
+++ Man In The Middle/Assets/Scripts/Intro2/XORController.cs
@@ -0,0 +1,32 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class XORController : MonoBehaviour
+{
+    public DOTweenAnimation[] showPuzzleAnims;
+    public RevealContinueButton but;
+
+    public GameObject puzzleObject;
+
+    private int correct = 0;
+
+    
+    public void ShowPuzzle() {
+        puzzleObject.SetActive(true);
+        for (int i = 0; i < showPuzzleAnims.Length; i++) {
+            showPuzzleAnims[i].DORestart();
+        }
+    }
+
+    public void addCorrect() {
+        correct += 1;
+
+        if (correct == 8) {
+            but.StartReveal();
+        }
+    }
+}
