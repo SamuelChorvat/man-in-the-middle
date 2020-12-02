d2764f891f396e2893c3aed8cd7324d82e801d59
Samuel Chorvat <samchorvat@gmail.com>
2020-12-02 16:21:11 +0000

    Started work on signatures

---
+++ Man In The Middle/Assets/Scripts/Intro4/PKCAnotControl.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PKCAnotControl : MonoBehaviour
+{
+    public GameObject anot1;
+    public GameObject anot2;
+
+    public void ShowAnot1() {
+        anot1.SetActive(true);
+        anot1.GetComponent<DOTweenAnimation>().DOPlay();
+    }
+
+    public void ShowAnot2() {
+        anot2.SetActive(true);
+        anot2.GetComponent<DOTweenAnimation>().DOPlay();
+    }
+
+}
