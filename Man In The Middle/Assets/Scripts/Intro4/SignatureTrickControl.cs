94ade6ddd50f66ef64f36b3e890faf4ce5c1a235
Samuel Chorvat <samchorvat@gmail.com>
2020-12-03 13:20:35 +0000

    added signature and certificate sections

---
+++ Man In The Middle/Assets/Scripts/Intro4/SignatureTrickControl.cs
@@ -0,0 +1,39 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class SignatureTrickControl : MonoBehaviour
+{
+    public GameObject signButton;
+    public GameObject messageRSA;
+
+    public RevealContinueButton but;
+
+    private void InitializeSign() {
+        signButton.GetComponent<RectTransform>().localScale = new Vector3(0, 0, 0);
+        messageRSA.GetComponent<RectTransform>().localScale = new Vector3(0, 0, 0);
+        signButton.SetActive(false);
+        messageRSA.SetActive(false);
+        messageRSA.GetComponent<TextMeshProUGUI>().text = "OWd8n/qSfBtNEkfzSG3hsemAj05EdJ0p41O3M2wfYhYw5CZ8NAm+rnkwGiBdZqnC2yBXLXCsfW9PMnsdCfQLoNYoR9oGmaqsa7gSNY1w31SISJq85q/WVu2Ce0ffKKJ1IDPXMrtVo4j0eu3/hvCOFA2MXqXxO/13uIjxO1fELhs=";
+    }
+
+    public void ShowMessage() {
+        InitializeSign();
+        messageRSA.SetActive(true);
+        messageRSA.GetComponent<DOTweenAnimation>().DOPlay();
+    }
+
+    public void ShowSignButton() {
+        signButton.SetActive(true);
+        signButton.GetComponent<DOTweenAnimation>().DOPlay();
+    }
+
+    public void OnSignClick() {
+        signButton.SetActive(false);
+        messageRSA.GetComponent<TextMeshProUGUI>().text = "<color=red>As this can be used to trick people into decrypting</color>";
+        but.StartReveal();
+    }
+}
