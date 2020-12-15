fb1fa6cfae98a6ad17c0055f2e2cde3023557762
Samuel Chorvat <samchorvat@gmail.com>
2020-12-15 11:58:06 +0000

    Finished authenticated encryption and therefore the Intro to Cryptography section

---
+++ Man In The Middle/Assets/Scripts/Intro4/KnownDemoControl.cs
@@ -0,0 +1,14 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnownDemoControl : MonoBehaviour
+{
+    public GameObject knownDemo;
+
+    public void ShowDemo() {
+        knownDemo.SetActive(true);
+        knownDemo.GetComponent<DOTweenAnimation>().DOPlay();
+    }
+}
