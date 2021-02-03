4c5c8b5e638692372206f9e39bc61de05007b101
Samuel Chorvat <samchorvat@gmail.com>
2021-02-03 13:10:15 +0000

    started work creaating framework for protocol attack puzzles

---
+++ Man In The Middle/Assets/Scripts/Protocols1/ProtocolStepController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtocolStepController : MonoBehaviour
+{
+    public GameObject alice;
+    public GameObject bob;
+    public GameObject carol;
+
+    public GameObject aliceBobMessage;
+    public GameObject bobAliceMessage;
+    public GameObject aliceCarolMessage;
+    public GameObject carolAliceMessage;
+    public GameObject carolBobMessage;
+    public GameObject bobCarolMessage;
+
+    public GameObject aliceBobArrow;
+    public GameObject bobAliceArrow;
+    public GameObject aliceCarolArrow;
+    public GameObject carolAliceArrow;
+    public GameObject carolBobArrow;
+    public GameObject bobCarolArrow;
+
+    public void ResetStep() {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
