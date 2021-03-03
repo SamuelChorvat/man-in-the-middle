d3bf599b8c62cce00b374eaf5c23c4a0d62eeabb
Samuel Chorvat <samchorvat@gmail.com>
2021-03-03 12:27:56 +0000

    started working on protocol attack 7

---
+++ Man In The Middle/Assets/Scripts/Protocols1/ProtocolAttack7Controller.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtocolAttack7Controller : MonoBehaviour
+{
+    [Header("Framework Controller")]
+    public GameObject protocolFramework;
+    public ProtocolFrameworkController frameworkControl;
+}
